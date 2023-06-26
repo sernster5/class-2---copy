364f43f9025d6e8e7b5f11ceba5f3a0ce464051a
sernster5 <137819609+sernster5@users.noreply.github.com>
2023-06-26 13:24:52 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+    basic.clearScreen()
+})
