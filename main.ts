a5c59decea26f59c7484ce9983110c18c3c4ea78
sernster5 <137819609+sernster5@users.noreply.github.com>
2023-06-26 13:33:37 -0400

    Added buttton A + B

--- main.ts
+++ main.ts
@@ -3,8 +3,35 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(500)
+        basic.showIcon(IconNames.Confused)
+        basic.pause(500)
+        basic.showIcon(IconNames.Surprised)
+        basic.pause(500)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    music.play(music.createSoundExpression(
+    WaveShape.Sine,
+    5000,
+    1,
+    216,
+    0,
+    6693,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Curve
+    ), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Surprised)
     basic.pause(500)
     basic.clearScreen()
 })
